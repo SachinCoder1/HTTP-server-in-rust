be4203b39d7edfccff7a3e4a130230e62c3c0732
Sachin Gurjar <98685642+SachinCoder1@users.noreply.github.com>
2023-08-18 14:56:39 +0530

    Server struct with new and run implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+
+    let server = Server::new("127.0.0.1:8080".to_string());
+    server.run();
+
+}
+
+struct Server { 
+    address: String,
+}
+
+impl Server {
+    fn new(address:String) -> Self {
+        Self {
+            address
+        }
+    }
+
+    fn run (self) {
+       println!("Server listening on {}", self.address);
+    }
 }
